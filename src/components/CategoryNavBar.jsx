8bb191489c0f3a38015817e949a17d4c6e8bdbc1
Gabriel O. <gabriele.kkj@gmail.com>
2025-08-05 10:38:31 +0200

    Added motion.div tag to the  CategoryNavBar inner elements.

--- src/components/CategoryNavBar.jsx
+++ src/components/CategoryNavBar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 export default function CategoryNavBar(){
 
-
-
-
     return (
-        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center mb-4 border-amber-50">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center mb-4 border-2 border-b-cyan-200 rounded-3xl">
+            <motion.div
+                whileHover={{ scale: 1.04 }}
+                className="bg-[var(--cl-surface0)] p-6 rounded-xl shadow text-amber-50"
+            >
             <Link to="/sport">
                 <div>
                     <img src="src/assets/sport-cat-icon.PNG" alt="sport" className="w-16 h-16"/>
                         Sport
                 </div>
             </Link>
+            </motion.div>
+            <motion.div
+                whileHover={{ scale: 1.02 }}
+                className="bg-[var(--cl-surface0)] p-6 rounded-xl shadow text-amber-50"
+            >
             <Link to="/kunst">
                 <div>
 
@@ -22,12 +29,22 @@ export default function CategoryNavBar(){
 
                 </div>
              </Link>
+            </motion.div>
+            <motion.div
+                whileHover={{ scale: 1.02 }}
+                className="bg-[var(--cl-surface0)] p-6 rounded-xl shadow text-amber-50"
+            >
             <Link to="/outdoor">
                 <div>
                     <img src="src/assets/outdoor-cat-icon.png" alt="outdoor" className="w-16 h-16"/>
-                    Outdoor-Aktivitäten
+                    Outdoor
                 </div>
             </Link>
+            </motion.div>
+            <motion.div
+                whileHover={{ scale: 1.02 }}
+                className="bg-[var(--cl-surface0)] p-6 rounded-xl shadow text-amber-50"
+            >
             <Link to="/sonsitges">
                 <div>
                     <img src="src/assets/sonstiges-icon.jpg" alt="sonstiges" className="w-16 h-16"/>
@@ -35,6 +52,7 @@ export default function CategoryNavBar(){
 
                 </div>
             </Link>
+            </motion.div>
         </div>
     )
 }
